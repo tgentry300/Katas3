e2e3fb400f862c4a47a179135c3661017f2f39eb
Taylor Gentry <taylor.gentry300@gmail.com>
2018-05-14 10:34:49 -0400

    resubmitted with headers to make easier to read

--- main.js
+++ main.js
@@ -5,11 +5,18 @@ function makediv(content) {
     div.textContent = content
     document.getElementById("container").appendChild(div)
 }
+function makeHeader(header) {
+    let div = document.createElement("H2")
+    div.textContent = header
+    document.getElementById("container").appendChild(div)
+}
 // Kata number 1
+makeHeader("Kata 1")
 for (let i = 0; i <= 20; i++) {
     makediv(i)
 }
 // Kata number 2
+makeHeader("Kata 2")
 for (i = 0; i <= 20; i++) {
     if (i % 2 == 0) {
         makediv(i)
@@ -17,6 +24,7 @@ for (i = 0; i <= 20; i++) {
 }
 
 // Kata number 3
+makeHeader("Kata 3")
 for (i = 0; i < 20; i++) {
     if (i % 2 == 1) {
         makediv(i)
@@ -24,69 +32,79 @@ for (i = 0; i < 20; i++) {
 }
 
 // Kata number 4
-for (i = 1; i <= 100; i+=5) {
-    if (i % 5 == 0) {
-        makediv(i)
-    }
+makeHeader("Kata 4")
+for (i = 0; i <= 100; i+=5) {
+    makediv(i)
 }
 
 // Kata number 5
+makeHeader("Kata 5")
 for (i = 1; i <= 100; i++) {
     var y = i * i
     if (y < 101)
-        makediv(y)
+    makediv(y)
 }
 
 // Kata number 6
+makeHeader("Kata 6")
 for (i = 20; i >= 1; i--)
-    makediv(i)
+makediv(i)
 // Kata number 7
+makeHeader("Kata 7")
 for (i = 20; i >= 2; i--) {
     if (i % 2 == 0)
-        makediv(i)
+    makediv(i)
 }
 
 // Kata number 8
+makeHeader("Kata 8")
 for (i = 20; i >= 1; i--) {
     if (i % 2 == 1)
-        makediv(i)
+    makediv(i)
 }
 
 // Kata number 9
+makeHeader("Kata 9")
 for (i = 100; i >= 5; i-=5) {
     if (i % 5 == 0)
-        makediv(i)
+    makediv(i)
 }
 
 // Kata number 10
+makeHeader("Kata 10")
 for (i = 100; i >= 1; i--) {
     var y = i * i
     if (y <= 100)
-        makediv(y)
+    makediv(y)
 }
 // Kata number 11
+makeHeader("Kata 11")
 {
     let i = sampleArray.toString();
     makediv(i)
 }
 // Kata number 12
+makeHeader("Kata 12")
 for (i = 0; i < sampleArray.length; i++) {
     if (sampleArray[i] % 2 == 0) {
         makediv(sampleArray[i])
     }
 }
 // Kata number 13
+makeHeader("Kata 13")
 for (i = 0; i < sampleArray.length; i++) {
     if (sampleArray[i] % 2 == 1) {
         makediv(sampleArray[i])
     }
 }
 // Kata number 14
+makeHeader("Kata 14")
 for (i = 0; i < sampleArray.length; i++) {
     let y = (sampleArray[i] * sampleArray[i])
     makediv(y)
 }
 // Kata number 15
+makeHeader("Kata 15")
 {
     const twentyArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     let sum = 0
@@ -96,6 +114,7 @@ for (i = 0; i < sampleArray.length; i++) {
     makediv(sum)
 }
 // Kata number 16
+makeHeader("Kata 16")
 {
     let sum = 0
     for (i = 0; i < sampleArray.length; i++) {
@@ -104,11 +123,12 @@ for (i = 0; i < sampleArray.length; i++) {
     makediv(sum)
 }
 // Kata number 17
+makeHeader("Kata 17")
 makediv(Math.min(...sampleArray))
 // Kata number 18
+makeHeader("Kata 18")
 makediv(Math.max(...sampleArray))
 // Kata number 19
-
 const dest = document.getElementById("place")
 
 function makebox() {
